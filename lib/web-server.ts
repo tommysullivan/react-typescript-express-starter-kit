87109f0a445e3cf0493bf2d6bd90ee14a3b5e1d6
Mike <mchen300@gmail.com>
2017-05-21 19:08:36 -0700

    added bootstrap. couldn't get it to work with installed files so used CDN links and commented out the filed attempt

--- lib/web-server.ts
+++ lib/web-server.ts
@@ -9,6 +9,9 @@ const myUrl = `http://${hostName}:${portNumber}`;
 expressApp.set('port', portNumber);
 expressApp.use(express.static('lib/public'));
 expressApp.use(express.static('dist/public'));
+// expressApp.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+// expressApp.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+// expressApp.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 http.createServer(expressApp).listen(
                 portNumber,
                 hostName,
